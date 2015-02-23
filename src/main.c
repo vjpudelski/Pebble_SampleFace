c46223ada9357633dca437ad54ac23726f866966
vjpudelski <vjpudelski@gmail.com>
2015-02-22 22:12:55 -0500

    inverted color and date added
    - inverted colors
    - added date to face

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 
 static void update_time() {
   // Get a tm structure
@@ -24,11 +25,39 @@ static void update_time() {
   text_layer_set_text(s_time_layer, buffer);
 }
 
+static void update_day() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00/00/0000";
+
+  // Write the current hours and minutes into the buffer
+  strftime(buffer, sizeof("00/00/0000"), "%m/%d/%Y", tick_time);
+  
+  // Display this time on the TextLayer
+  text_layer_set_text(s_date_layer, buffer);
+}
+
 static void main_window_load(Window *window) {
+  // Create date TextLayer
+  s_date_layer = text_layer_create(GRect(0, 105, 144, 50));
+  text_layer_set_background_color(s_date_layer, GColorBlack);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text(s_date_layer, "00/00/0000");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
 
   // Improve the layout to be more like a watchface
@@ -48,9 +77,15 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
 
+static void day_handler(struct tm *tick_day, TimeUnits units_changed) {
+  update_day();
+}
+
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
+  
+  window_set_background_color(s_main_window, GColorBlack);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -63,9 +98,11 @@ static void init() {
   
   // Make sure the time is displayed from the start
   update_time();
+  update_day();
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(DAY_UNIT, day_handler);
 }
 
 static void deinit() {
